3980b3f82d48fb1b31bd16ea57f88f1ea556bac5
andreadorazio <andrea.dorazio200187@gmail.com>
2023-05-20 15:32:37 +0200

    Evento serializer e deserializer

--- src/main/java/org/univaq/swa/auleweb/aulewebrest/jackson/EventoDeserializer.java
+++ src/main/java/org/univaq/swa/auleweb/aulewebrest/jackson/EventoDeserializer.java
@@ -1,13 +1,60 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+*/
 package org.univaq.swa.auleweb.aulewebrest.jackson;
 
 /**
- *
- * @author mttpe
- */
+*
+* @author mttpe
+*/
 public class EventoDeserializer {
-    
+
+   @Override
+   public Evento deserialize (JsonParser jp, DeserializationContext ctxt)throws IOException, JsonProcessingException{
+       Evento e = new Evento();
+
+       JsonNode node = jp.getCodec().readTree(jp);
+
+       if (node.has("data")) {
+           e.setData(node.get("data").asText());
+       }
+
+       if (node.has("oraInizio")) {
+           e.setOraInizio(node.get("oraInizio").asText());
+       }
+       if (node.has("oraFine")) {
+           e.setOraFine(node.get("oraFine").asText());
+       }
+       if (node.has("descrizione")) {
+             e.setDescrizione(node.get("descrizione").asText());
+       }
+       if (node.has("nome")) {
+           e.setNome(node.get("nome").asText());
+       }
+       if (node.has("id")) {
+           e.setId(node.get("id").asInt());
+       }
+
+       if (node.has("aule")) {
+           JsonNode ne = node.get("aule");
+           List<Aula> aule = new ArrayList<>();
+           e.setAule(aule);
+           for (int i = 0; i < ne.size(); ++i) {
+              aule.add(jp.getCodec().treeToValue(ne.get(i), Aula.class));
+           }          
+       }
+
+       if (node.has("tipo")){
+           e.setTipologia(jp.getCodec().treeToValue(node.get("tipo"), Tipologia.class));
+       }
+       if (node.has("responsabile")){
+           e.setResponsabile(jp.getCodec().treeToValue(node.get("responsabile"),Responsabile.class));
+       }
+
+       return e;
+   }
+
+
+
 }
--- src/main/java/org/univaq/swa/auleweb/aulewebrest/jackson/EventoSerializer.java
+++ src/main/java/org/univaq/swa/auleweb/aulewebrest/jackson/EventoSerializer.java
@@ -1,13 +1,29 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+*/
 package org.univaq.swa.auleweb.aulewebrest.jackson;
 
 /**
- *
- * @author mttpe
- */
-public class EventoSerializer {
-    
+*
+* @author mttpe
+*/
+public class EventoSerializer extends JsonSerializer<Evento> {
+
+   @Override
+   public void serialize(Evento item, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+
+       jgen.writeStartObject(); // {
+       jgen.writeStringField("data", item.getData());
+       jgen.writeStringField("oraInizio", item.getOraInizio());
+       jgen.writeStringField("oraFine", item.getOraFine());
+       jgen.writeStringField("descrizione", item.getDescrizione());
+       jgen.writeStringField("nome", item.getNome());
+       jgen.writeStringField("note", item.getNote());
+       jgen.writeNumberField("id", item.getId());
+       jgen.writeObjectField("tipo", item.getTipo());
+       jgen.writeObjectField("aule", item.getAule());
+       jgen.writeObjectField("responsabile", item.getResponsabile());
+       jgen.writeEndObject(); // }
+   }
 }
