07d104e35fcdd2858e3eec4999563ec5ec27056d
andreadorazio <andrea.dorazio200187@gmail.com>
2023-05-28 11:05:16 +0200

    dao responsabile

--- src/main/java/com/stdt/auleweb/data/dao/ResponsabileDAO_MySQL.java
+++ src/main/java/com/stdt/auleweb/data/dao/ResponsabileDAO_MySQL.java
@@ -5,30 +5,32 @@ import com.stdt.auleweb.framework.data.DataException;
 import com.stdt.auleweb.framework.data.DataLayer;
 import com.stdt.auleweb.data.model.Evento;
 import com.stdt.auleweb.template.model.Responsabile;
+import com.stdt.auleweb.data.proxy.ResponsabileProxy;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ResponsabileDAO_MySQL extends DAO implements ResponsabileDAO{
 
-    private PreparedStatement sAulaByID;
-    private PreparedStatement sAule, sAulaByPosizione;
-    private PreparedStatement iAula, uAula, dAula;
+    private PreparedStatement sResponsabileByID;
+    private PreparedStatement sResponsabileByEvento;
+    private PreparedStatement iResponsabile, uResponsabile, dResponsabile;
     
     public ResponsabileDAO_MySQL(DataLayer d) {
         super(d);
     }
     
-        @Override
+    @Override
     public void init() throws DataException {
         try {
             super.init();
 
             //precompiliamo tutte le query utilizzate nella classe
             //precompile all the queries uses in this class
-            sAulaByID = connection.prepareStatement("SELECT * FROM aula WHERE ID=?");
+            sResponsabileByID = connection.prepareStatement("SELECT * FROM Responsabile WHERE ID=?");
             //++++++sAulaByPosizione = connection.prepareStatement("SELECT ID AS aulaID FROM aula WHERE posizioneID=?");
             //++++++sAule = connection.prepareStatement("SELECT ID AS aulaID FROM aula");
-
+            sResponsabileByEvento=connection.prepareStatement("SELECT r.* FROM Responsabile as r,Evento WHERE Evento.ID=?");
             //notare l'ultimo paametro extra di questa chiamata a
             //prepareStatement: lo usiamo per assicurarci che il JDBC
             //restituisca la chiave generata automaticamente per il
@@ -40,7 +42,9 @@ public class ResponsabileDAO_MySQL extends DAO implements ResponsabileDAO{
            // iAula = connection.prepareStatement("INSERT INTO article (title,text,authorID,issueID,page) VALUES(?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
             //uAula = connection.prepareStatement("UPDATE article SET title=?,text=?,authorID=?,issueID=?, page=?, version=? WHERE ID=? and version=?");
             //dAula = connection.prepareStatement("DELETE FROM article WHERE ID=?");
-
+            iResponsabile = connection.prepareStatement("INSERT INTO Responsabile (nome,email) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);
+            uResponsabile = connection.prepareStatement("UPDATE Responsabile SET nome=?, email=?, version=? WHERE ID=? and version=?");
+            dResponsabile = connection.prepareStatement("DELETE FROM Responsabile WHERE ID=?");
         } catch (SQLException ex) {
             throw new DataException("Error initializing auleweb data layer", ex);
         }
@@ -51,15 +55,11 @@ public class ResponsabileDAO_MySQL extends DAO implements ResponsabileDAO{
         //anche chiudere i PreparedStamenent � una buona pratica...
         //also closing PreparedStamenents is a good practice...
         try {
-
-            sAulaByID.close();
-
-            sAulaByPosizione.close();
-            sAule.close();
-
-            iAula.close();
-            uAula.close();
-            dAula.close();
+            sResponsabileByID.close();
+            sResponsabileByEvento.close();
+            iResponsabile.close();
+            uResponsabile.close();
+            dResponsabile.close();
 
         } catch (SQLException ex) {
             //
@@ -69,8 +69,9 @@ public class ResponsabileDAO_MySQL extends DAO implements ResponsabileDAO{
 
     @Override
     public Responsabile createResponsabile() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return new ResponsabileProxy(getDataLayer());
     }
+    
 
     @Override
     public Responsabile getResponsabileByEvento(Evento evento) throws DataException {
---
+++ src/main/java/com/stdt/auleweb/data/proxy/ResponsabileProxy.java
@@ -0,0 +1,84 @@
+package com.stdt.auleweb.data.proxy;
+
+import com.stdt.auleweb.data.dao.AttrezzaturaDAO;
+import com.stdt.auleweb.data.dao.EventoDAO;
+import com.stdt.auleweb.data.dao.GruppoDAO;
+import com.stdt.auleweb.data.dao.PosizioneDAO;
+import com.stdt.auleweb.data.impl.AulaImpl;
+import com.stdt.auleweb.data.impl.ResponsabileImpl;
+import com.stdt.auleweb.data.model.Attrezzatura;
+import com.stdt.auleweb.data.model.Evento;
+import com.stdt.auleweb.data.model.Gruppo;
+import com.stdt.auleweb.framework.data.DataException;
+import com.stdt.auleweb.framework.data.DataItemProxy;
+import com.stdt.auleweb.framework.data.DataLayer;
+import com.stdt.auleweb.data.model.Posizione;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ResponsabileProxy extends ResponsabileImpl implements DataItemProxy {
+    protected boolean modified;
+   
+
+    protected DataLayer dataLayer;
+
+    public ResponsabileProxy(DataLayer d)
+    {
+        super();
+        //dependency injection
+        this.dataLayer = d;
+        this.modified = false;
+    }
+    
+    @Override
+    public void setKey(Integer key) {
+        super.setKey(key);
+        this.modified = true;
+    }
+    @Override
+    public void setNome(String nome) {
+        super.setNome(nome);
+        this.modified = true;
+    }
+    @Override
+    public void setEmailResponsabile(String emailResponsabile) {
+        super.setEmailResponsabile(emailResponsabile);
+        this.modified = true;
+    }
+   @Override
+    public List<Evento> getEventi() {
+        if (super.getEventi() == null) {
+            try {
+                super.setEventi(((EventoDAO) dataLayer.getDAO(Evento.class)).getEventiByResponsabile(this));
+            } catch (DataException ex) {
+                Logger.getLogger(ResponsabileProxy.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return super.getEventi();
+
+    }
+
+    @Override
+    public void addEventi(Evento evento) {
+        super.addEventi(evento);
+        this.modified = true;
+    }
+
+    @Override
+    public void setEventi(List<Evento> eventi) {
+        super.setEventi(eventi);
+        this.modified = true;
+    }
+    
+    @Override
+    public void setModified(boolean dirty) {
+        this.modified = dirty;
+    }
+
+    @Override
+    public boolean isModified() {
+        return modified;
+    }
+    
+}
