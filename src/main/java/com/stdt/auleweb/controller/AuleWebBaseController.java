44fc7a333b15a30d54536dc224905653f62bed53
Timat-P <mttpecc@gmail.com>
2023-05-30 15:30:29 +0200

    Aggiunti file mancanti dal progetto Newspaper

---
+++ src/main/java/com/stdt/auleweb/controller/AuleWebBaseController.java
@@ -0,0 +1,24 @@
+package com.stdt.auleweb.controller;
+import com.stdt.auleweb.data.dao.AuleWebDataLayer;
+import com.stdt.auleweb.framework.controller.AbstractBaseController;
+import com.stdt.auleweb.framework.data.DataLayer;
+import java.sql.SQLException;
+import jakarta.servlet.ServletException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Giuseppe Della Penna
+ */
+public abstract class AuleWebBaseController extends AbstractBaseController {
+
+    @Override
+    protected DataLayer createDataLayer(DataSource ds) throws ServletException {
+        try {
+            return new AuleWebDataLayer(ds);
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+}
