8409371c4b6dc1dc4ec575ac03a2f3f2f9eeef01
Timat-P <mttpecc@gmail.com>
2023-06-18 14:04:34 +0200

    Modificati nomi delle servlet di gestione aule e gruppi

--- src/main/java/com/stdt/auleweb/controller/GestioneAule.java
+++ src/main/java/com/stdt/auleweb/controller/GestioneAule.java
@@ -1,10 +1,13 @@
 package com.stdt.auleweb.controller;
 
 import com.stdt.auleweb.data.dao.AuleWebDataLayer;
+import com.stdt.auleweb.data.model.Gruppo;
 import com.stdt.auleweb.framework.data.DataException;
 import com.stdt.auleweb.framework.result.TemplateManagerException;
 import com.stdt.auleweb.framework.result.TemplateResult;
+import com.stdt.auleweb.framework.security.SecurityHelpers;
 import java.io.IOException;
+import java.time.LocalDate;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,14 +16,19 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Giuseppe
  */
-public class SelezioneGruppo extends AuleWebBaseController {
+public class GestioneAule extends AuleWebBaseController {
 
-    private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+    private void action_default(HttpServletRequest request, HttpServletResponse response, int IDgruppo) throws IOException, ServletException, TemplateManagerException {
+
         try {
+            Gruppo gruppo = ((AuleWebDataLayer) request.getAttribute("datalayer")).getGruppoDAO().getGruppo(IDgruppo);
             TemplateResult res = new TemplateResult(getServletContext());
             request.setAttribute("page_title", "Gruppi");
-            request.setAttribute(("gruppi"), ((AuleWebDataLayer) request.getAttribute("datalayer")).getGruppoDAO().getGruppi());
-            res.activate("selezione_gruppo.ftl.html", request, response);
+
+            request.setAttribute(("data"), LocalDate.now());
+            request.setAttribute(("aule"), ((AuleWebDataLayer) request.getAttribute("datalayer")).getAulaDAO().getAuleByGruppo(gruppo));
+            request.setAttribute(("gruppo"), gruppo);
+            res.activate("gestione_aule.ftl.html", request, response);
         } catch (DataException ex) {
             handleError("Data access exception: " + ex.getMessage(), request, response);
         }
@@ -30,8 +38,10 @@ public class SelezioneGruppo extends AuleWebBaseController {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException {
 
+        int IDgruppo;
         try {
-            action_default(request, response);
+            IDgruppo = SecurityHelpers.checkNumeric(request.getParameter("IDgruppo"));
+            action_default(request, response, IDgruppo);
 
         } catch (IOException | TemplateManagerException ex) {
             handleError(ex, request, response);
