7ef73cc3fadfa682f5d73cf9b614e7eb89f2d05f
AlessiaSeba <alessia.sebastiano22@gmail.com>
2023-07-03 10:27:28 +0200

    tabella funzionante

--- databaseSTDT.sql
+++ databaseSTDT.sql
@@ -214,11 +214,15 @@ insert into Amministratore(username, `password`) values
     ("Seba", "admin");
     
 insert into Evento(giorno, oraInizio, oraFine, nome, descrizione, tipologia, responsabileID, aulaID, corsoID) values
-	("2023-06-16", "22:00", "13:30", "I2I007","docente mascio tania", "lezione",2,1,1),
-    ("2023-06-16", "00:30", "17:30", "DT0761", "docente masi anna" , "lezione",2,1,2),
-    ("2023-06-16", "14:30", "14:45", "F0140","docente smriglio stefano", "lezione", 1,1,2),
-    ("2023-05-16", "18:00", "18:30", "DT0180","docente della penna giuseppe", "lezione", 1,1,1),
-    ("2023-06-17", "18:30", "18:45", "F0140","docente smriglio stefano", "lezione", 1,1,2);
+	("2023-06-14", "18:00", "18:30", "I2I007","docente mascio tania", "lezione",2,1,1),
+    ("2023-06-15", "16:30", "17:30", "DT0761", "docente masi anna" , "lezione",2,1,2),
+    ("2023-06-17", "15:30", "16:00", "F0140","docente smriglio stefano", "lezione", 1,1,2),
+    ("2023-06-16", "18:00", "18:30", "DT0180","docente della penna giuseppe", "lezione", 1,1,1),
+    ("2023-06-17", "18:30", "18:45", "F0140","docente smriglio stefano", "lezione", 1,1,2),
+	("2023-07-01", "18:30", "18:45", "vero","docente smriglio stefano", "lezione", 1,1,2),
+    ("2023-07-01", "17:30", "18:45", "falso","docente mascio tania", "lezione", 1,1,2),
+	("2023-07-01", "19:30", "19:45", "vero1","docente masi anna", "lezione", 1,1,2);
+   
     
 insert into Corso(nome) values
 	("Advanced database system (basi di dati II)"),
@@ -269,3 +273,6 @@ insert into Richiede (eventoID, corsoID) values
 
 
 SELECT * from evento
+
+
+    
